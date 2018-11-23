2731ed6d73c56122a52e05e0fe65b5be270ef26f
Sébastien LAURENT <45282732+slabs-forge@users.noreply.github.com>
2018-11-23 20:42:24 +0100

    Gradle wrapper added

---
+++ src/main/java/net/slabs/starter/ApplicationStartup.java
@@ -0,0 +1,14 @@
+package net.slabs.starter;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationStartup implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) {
+
+    }
+}
